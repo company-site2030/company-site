6324b3342280437456e9d93e58dfce78e924846c
USDT2030 <fadifadi203074@gmail.com>
2025-09-25 14:25:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ links.forEach(link => {
     navLinks.classList.remove('active');
   });
 });
+
+// التعامل مع الفورمات
+document.addEventListener("DOMContentLoaded", () => {
+  const forms = document.querySelectorAll("form");
+  
+  forms.forEach(form => {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault(); // يمنع إعادة تحميل الصفحة
+      alert("✅ تم إرسال البيانات بنجاح!");
+      form.reset(); // يمسح البيانات من الفورم بعد الإرسال
+    });
+  });
+});
